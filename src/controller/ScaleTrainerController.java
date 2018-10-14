f9ba9dfee23b5f22749318a644962a3402456566
blex <AlexRottlaender@gmail.com>
2018-10-13 20:15:46 -0600

    Initial commit: Controller, Runner, added to .gitignore

---
+++ src/controller/ScaleTrainerController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class ScaleTrainerController 
+{
+	public void start()
+	{
+		
+		
+		
+		
+	}
+	
+	
+	
+	//*******PRIVATE*METHODS*******
+	
+	
+	
+	
+	
+	
+}
---
+++ src/controller/ScaleTrainerRunner.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class ScaleTrainerRunner 
+{
+	public static void main(String [] args)
+	{
+		ScaleTrainerController app = new ScaleTrainerController();
+		app.start();
+	}
+
+}
